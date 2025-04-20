634220097b743c699cd8f0e5e4b65af36361fa52
chryswds <dybas.chrys@gmail.com>
2025-04-20 17:05:49 +0100

    changing config

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,18 @@ export default defineConfig({
       entry: "electron/main.cjs",
     }),
   ],
+  base: process.env.ELECTRON === "true" ? "./" : "/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 });
